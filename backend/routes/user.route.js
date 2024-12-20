72ca6d5e6ebd05977337d07d00f64fac97a2ee6f
Arpita24341213 <arpitatinni115@gmail.com>
2024-12-21 04:52:39 +0600

    backend updated

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { register, login, logout, updateProfile } from "../models/controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", singleUpload, register);
 router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
 router.post("/profile/update", isAuthenticated, updateProfile);
